a8fb97ef2c3ce16d1bfe17c4bffc7a6e2c46e201
Stephen Oba <obastepheno@gmail.com>
2020-02-28 17:06:23 +0100

    set Statc root

--- twitter_contest/settings.py
+++ twitter_contest/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://ai6phtwittercontest.herokuapp.com/ "]
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
