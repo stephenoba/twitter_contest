99ecf5801bdc91bd6a29f889d61ae8d9577d62ef
Stephen Oba <obastepheno@gmail.com>
2020-03-02 09:35:49 +0100

    alter field tweet in Tweet to Textfield

--- play_ground/models.py
+++ play_ground/models.py
@@ -16,7 +16,7 @@ class Contestant(models.Model):
 
 class Tweet(models.Model):
     twitter_handle = models.ForeignKey(Contestant, on_delete=models.CASCADE)
-    tweet = models.CharField(max_length=200)
+    tweet = models.TextField(blank=True)
     time_stamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
